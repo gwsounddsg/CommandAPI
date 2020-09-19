54f28efa796411edbbaab815d62d0c9745308a6f
GW <gwsounddsg@gmail.com>
2020-09-19 15:09:25 -0700

    adds first test for checking 0 items in db for controller

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -6,7 +6,7 @@ using CommandAPI.Models;
 
 
 
-namespace CommandAPI.Controller
+namespace CommandAPI.Controllers
 {
 
     [ApiController]
--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -1,12 +1,53 @@
 using NUnit.Framework;
 using CommandAPI.Controllers;
 using CommandAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace CommandAPI.Tests
 {
     public class CommandsControllerTests
     {
-        
+        DbContextOptionsBuilder<CommandContext> optionsBuilder;
+        CommandContext dbContext;
+        CommandsController controller;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            optionsBuilder = new DbContextOptionsBuilder<CommandContext>();
+            optionsBuilder.UseInMemoryDatabase("UnitTestInMemBD");
+            dbContext = new CommandContext(optionsBuilder.Options);
+
+            controller = new CommandsController(dbContext);
+        }
+
+
+        [TearDown]
+        public void Destroy()
+        {
+            optionsBuilder = null;
+
+            foreach (var cmd in dbContext.CommandItems)
+            {
+                dbContext.CommandItems.Remove(cmd);
+            }
+
+            dbContext.SaveChanges();
+            dbContext.Dispose();
+            controller = null;
+        }
+
+
+        [Test]
+        public void GetCommandItems_ReturnsZeroItems_WhenDBIsEmpty()
+        {
+            // call
+            var result = controller.GetCommandItems();
+
+            // verify
+            Assert.IsEmpty(result.Value);
+        }
     }
 }
